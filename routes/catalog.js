3cd637b9b1ca4e046c0e22e1e95a32b2c0e442b0
Diogo Henriques <diogui@Orion.local>
2019-02-01 09:55:21 +0000

    Renamed genreController import

--- routes/catalog.js
+++ routes/catalog.js
@@ -6,7 +6,7 @@ var router = express.Router();
 var book_controller = require('../controllers/bookController');
 var author_controller = require('../controllers/authorController');
 var book_instance_controller = require('../controllers/bookInstanceController');
-var genre_controller = require('../controllers/GenreController');
+var genre_controller = require('../controllers/genreController');
 
 //Book Routes
 
